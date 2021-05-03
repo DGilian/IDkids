7b2cc161d7488371a7c69730615eee25163ec0a3
gilian <gilian@mailfence.com>
2021-05-03 13:20:19 +0200

    refresh new answers when user click on next button

--- game.js
+++ game.js
@@ -24,5 +24,23 @@ function nextQuestion() {
 
 function refreshQuestion() {
   const { questions, currentQuestion } = storeApp
+
+  const answerList = document.getElementById('answersList')
+  // refresh question
   document.getElementById('question').innerHTML = questions[currentQuestion].question
+
+  // remove old answers questions
+  answerList.innerHTML =''
+
+  // add new answers
+  questions[currentQuestion].answers.map(answer => {
+    const label = document.createElement("label");
+    const radio = document.createElement("input");
+    radio.type = "radio";
+    radio.name = "answer"
+    radio.value = answer;
+    answerList.appendChild(radio)
+    answerList.appendChild(label);
+    label.appendChild(document.createTextNode(answer));
+  })
 }
