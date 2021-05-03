f10eb891773f6fa58a1547d3c862515d7870541e
gilian <gilian@mailfence.com>
2021-05-03 11:56:04 +0200

    css on question container and add next button

--- game.js
+++ game.js
@@ -4,3 +4,7 @@ function startGame() {
   document.getElementById("startGame").className="hidden"
   document.getElementById("game").classList.remove("hidden")
 }
+
+function nextQuestion() {
+  console.log("next")
+}
