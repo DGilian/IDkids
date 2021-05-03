b28f2dc1d42d9af0c3f80bb74f562cf31b5d9c4b
gilian <gilian@mailfence.com>
2021-05-03 14:18:57 +0200

    add end game feature

--- game.js
+++ game.js
@@ -18,6 +18,7 @@ function startGame() {
 }
 
 function nextQuestion() {
+  if(storeApp.currentQuestion === 4) return endGame()
   const userAnswer = document.querySelector('input[name=answer]:checked').value;
   if(userAnswer === storeApp.questions[storeApp.currentQuestion].correct_answer) storeApp.goodAnswer +=1
   storeApp.currentQuestion += 1
@@ -34,6 +35,7 @@ function refreshQuestion() {
   const { questions, currentQuestion } = storeApp
 
   const answerList = document.getElementById('answersList')
+
   // refresh question
   document.getElementById('question').innerHTML = questions[currentQuestion].question
 
@@ -51,4 +53,22 @@ function refreshQuestion() {
     answerList.appendChild(label);
     label.appendChild(document.createTextNode(answer));
   })
+}
+
+function endGame() {
+  // reset block
+  const questionContainer = document.getElementById("questionContainer")
+  questionContainer.innerHTML =''
+
+  // add end message
+  const endMessage = document.createElement("p");
+  questionContainer.appendChild(endMessage)
+  endMessage.appendChild(document.createTextNode(`Congratulation you have ${storeApp.goodAnswer} good answer`))
+
+  // add restart button
+  const restartButton = document.createElement('button');
+  restartButton.innerHTML = 'restart game'
+  restartButton.onclick = () => window.location.reload()
+  restartButton.classList.add("resetButton")
+  questionContainer.appendChild(restartButton)
 }
